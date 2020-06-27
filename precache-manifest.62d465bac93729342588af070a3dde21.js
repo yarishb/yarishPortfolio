4add70623ed417c8cfffd06cd290d7ab9c83115b
Bogdan050104 <bogdan040105@gmail.com>
2020-06-27 20:20:31 +0300

    Updates

---
+++ precache-manifest.62d465bac93729342588af070a3dde21.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a29afaf0eaef9a1480fbda19cb799f8",
+    "url": "/yarishPortfolio/index.html"
+  },
+  {
+    "revision": "21db1b32634a15726967",
+    "url": "/yarishPortfolio/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "24849b4e7450247662da",
+    "url": "/yarishPortfolio/static/css/main.dc19788e.chunk.css"
+  },
+  {
+    "revision": "21db1b32634a15726967",
+    "url": "/yarishPortfolio/static/js/2.0f55f791.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/yarishPortfolio/static/js/2.0f55f791.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24849b4e7450247662da",
+    "url": "/yarishPortfolio/static/js/main.36f077b8.chunk.js"
+  },
+  {
+    "revision": "69a085bce7516976c862",
+    "url": "/yarishPortfolio/static/js/runtime-main.a15dd08d.js"
+  },
+  {
+    "revision": "ca9342c0de688134bd936fcebcd78302",
+    "url": "/yarishPortfolio/static/media/Screenshot_1 (1).ca9342c0.png"
+  },
+  {
+    "revision": "08f9b4bb4d2c743d82fa98cb33916af2",
+    "url": "/yarishPortfolio/static/media/Screenshot_2.08f9b4bb.png"
+  }
+]);
